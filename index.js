94b2bd898ef384340e1e0e77ef7c1297164da6cc
Pier Bover <226483+PierBover@users.noreply.github.com>
2022-08-17 16:46:15 -0500

    fix callback index bug and other improvements

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const callbacks = [];
+
 let currentFrame = 0;
+let requestId;
+let callbackIndex = 0;
 
 function checkCallbacks () {
 	currentFrame ++;
-
-	let allNull = true;
 
 	for (var i = 0; i < callbacks.length; i++) {
 		if (!callbacks[i]) continue;
@@ -12,33 +13,47 @@ function checkCallbacks () {
 		if (callbacks[i].targetFrame === currentFrame) {
 			callbacks[i].callback();
 			callbacks[i] = null;
-		} else {
-			allNull = false;
 		}
 	}
 
+	const allNull = callbacks.every((callback) => callback === null);
+
 	// If no pending callbacks reset everything
+
 	if (allNull) {
-		callbacks.splice(0, callbacks.length);
+		callbacks.length = 0;
 		currentFrame = 0;
 	} else {
-		window.requestAnimationFrame(checkCallbacks);
+		requestId = window.requestAnimationFrame(checkCallbacks);
 	}
 }
 
 export function setFrameout (callback, frames) {
-	const length = callbacks.push({
+
+	if (!callback) throw 'You must pass a callback';
+	if (typeof callback !== 'function') throw 'Callback must be a function';
+	if (!Number.isInteger(frames)) throw 'The number of frames must be an integer';
+	if (frames <= 0) throw 'The number of frames must be 1 or bigger';
+
+	// clear pending requests
+	window.cancelAnimationFrame(requestId);
+
+	// create a new callbackIndex
+	callbackIndex ++;
+
+	callbacks.push({
 		targetFrame: currentFrame + frames,
-		callback
+		callback,
+		index: callbackIndex
 	});
 
-	// Only do this on the first added callback
-	if (length === 1) window.requestAnimationFrame(checkCallbacks);
+	requestId = window.requestAnimationFrame(checkCallbacks);
 
 	// return index for clearFrameout()
-	return length - 1;
+	return callbackIndex;
 }
 
 export function clearFrameout (index) {
-	if (callbacks[index]) callbacks[index] = null;
+	const arrayIndex = callbacks.findIndex((item) => item.index === index);
+	if (arrayIndex >= 0) callbacks[arrayIndex] = null;
 }
